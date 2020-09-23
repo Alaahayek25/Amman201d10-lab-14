9ed04a2c67b8501c051e4f05d051534b0ea5ef17
AyaAbuRashed <ayanaser402@gmail.com>
2020-09-23 17:09:09 +0300

    lab14 done

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,8 @@
 
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 var table = document.getElementById('cart');
+var tboadyEl = document.getElementsByTagName('tbody');
+
 table.addEventListener('click', removeItemFromCart);
 var cart;
 
@@ -19,17 +21,41 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+
+while(tboadyEl.firstChild){
+ tboadyEl.deleteRow(0);
+
+}
+
+
+}
+
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+  for(var i =0 ;i < Cart.length;i++){
+
+      var rawEl = document.createElement('tr');
+      tboadyEl.appendChild(rawEl);
+      var deleteTd = document.createElement('td');
+      deleteTd.setAttribute('id', 'delRow');
+      var quantityTd = document.createElement('td');
+      var itemTd = document.createElement('td');
+      rawEl.appendChild(deleteTd);
+      rawEl.appendChild(quantityTd);
+      rawEl.appendChild(itemTd);
+      deleteTd.textContent="X";
+      quantityTd.textContent= Cart.items[i][quantity];
+      itemTd.textContent = Cart.items[i][product];
+  }
 
 }
 
@@ -38,6 +64,11 @@ function removeItemFromCart(event) {
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
+
+  if (event.target.id === 'delRow'){
+    Cart.removeItem()
+
+  }
 
 }
 
